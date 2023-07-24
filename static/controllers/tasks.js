498271f846f2ddd99e1d11153db5724a2168c95c
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-24 15:31:34 +0530

    merge conflicts resolution

--- static/controllers/tasks.js
+++ static/controllers/tasks.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const getAllTasks = (req, res) => {
     res.send(' Get All Items');
   };
@@ -16,22 +15,3 @@ const getAllTasks = (req, res) => {
   };
 
   module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
-=======
-const getAllTasks = (req, res) => {
-  res.send(' Get All Items');
-};
-const createTask = (req, res) => {
-  res.send('create Task');
-};
-const getTask = (req, res) => {
-  res.send('get single Task');
-};
-const updateTask = (req, res) => {
-  res.send('update Task');
-};
-const deleteTask = (req, res) => {
-  res.send('delete Task');
-};
-
-module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
->>>>>>> 5adb36f6f7964dc04d8d29cc29785461d2e45feb
